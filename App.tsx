f2b1ac4ad5c1ae9e621e65747bf8496149dcf302
kidstone <hyuksu0308@naver.com>
2023-04-20 16:19:45 +0900

    View, Text

--- App.tsx
+++ App.tsx
@@ -6,49 +6,26 @@
  */
 
 import React, {Component, useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import PropsChild from './propsChild'
 const App = () => {
-
-  const [sampleText, setSampleText] = useState('Hello World')
-  const [sampleBoolean, setSampleBoolean] = useState(true)
-  const [sampleNum, setSampleNum] = useState(0)
-  
-  const inputText = () => {
-    return sampleBoolean ? (
-    <Text>sampleBoolean is true</Text>
-    ) : (
-    <Text>sampleBoolean is false</Text>
-    )
-  }
-
-  const changeState = () => {
-    if (!sampleBoolean) {
-      setSampleText('Text Changed!')
-      setSampleBoolean(true)
-    } else {
-      setSampleText('Hello World')
-      setSampleBoolean(false)
-    }
-  }
-
-  const onAdd = () => {
-    setSampleNum(prevSampleNum => prevSampleNum + 1)
-  }
   return (
-    <View style={styles.background}>
-      <PropsChild sampleText={sampleText} changeState={changeState}/>
+    <View>
+      <View>
+        <Text>hello world</Text>
+      </View>
+      <View>
+        <Text>hello world</Text>
+      </View>
+      <View>
+        <Text>hello world</Text>
+      </View>
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+
 })
 export default App;
