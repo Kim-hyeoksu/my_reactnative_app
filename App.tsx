f858937fdfc5d512206f7069a3dc6ae8fb6347cd
kidstone <hyuksu0308@naver.com>
2023-04-20 17:07:56 +0900

    style

--- App.tsx
+++ App.tsx
@@ -10,15 +10,15 @@ import {View, Text, StyleSheet} from 'react-native';
 import PropsChild from './propsChild'
 const App = () => {
   return (
-    <View>
-      <View>
+    <View style={styles.mainView}>
+      <View style={styles.subView}>
+        <Text style={styles.mainText}>hello world</Text>
+      </View>
+      <View style={styles.subView}>
         <Text>hello world</Text>
       </View>
-      <View>
-        <Text>hello world</Text>
-      </View>
-      <View>
-        <Text>hello world</Text>
+      <View style={styles.anotherView}>
+        <Text style={styles.mainText}>hello world</Text>
       </View>
     </View>
   )
@@ -26,6 +26,32 @@ const App = () => {
 
 
 const styles = StyleSheet.create({
-
+  mainView: {
+    backgroundColor: 'green',
+    height: '100%',
+    paddingTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  subView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '50%'
+  },
+  anotherView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mainText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'red',
+    padding: 20
+  }
 })
 export default App;
