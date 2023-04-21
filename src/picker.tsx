8852806e5d1e078e8b68086d6998a8edb50863f3
kidstone <hyuksu0308@naver.com>
2023-04-22 03:45:50 +0900

    Slider

--- src/picker.tsx
+++ src/picker.tsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react'
 import {View, Text, StyleSheet, TextInput} from 'react-native'
 import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
 interface Props {
 }
 const PickerComponent = () => {  //소괄호는 jsx를 리턴할 때 사용 
 
   const [country, setCountry] = useState('canada')
+  const [value, setValue] = useState(50)
+
+  const sliderValueChange = (value)=> {
+    setValue(value)
+  }
   return (
     <View style={styles.container}>
+      <Slider 
+      style={{height: 40, width: 300}}
+      value={value}
+      minimumValue={0}
+      maximumValue={100}
+      onValueChange={sliderValueChange}
+      maximumTrackTintColor='red'
+      minimumTrackTintColor='blue'
+      step={1}
+      />
+      <Text style={styles.input}>{value}</Text>
       <Picker
-      style={{
-        height: 50,
-        width: 250
-      }}
+      style={{marginTop: 20, height: 40, width: 250, backgroundColor:'green'}}
       selectedValue={country}
       onValueChange={(val, idx)=>setCountry(val)}
       >
@@ -30,6 +44,10 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     marginBottom: 200,
     alignItems: 'center'
+  },
+  input: {
+    fontSize: 25,
+    height: 30
   }
 })
 export default PickerComponent
